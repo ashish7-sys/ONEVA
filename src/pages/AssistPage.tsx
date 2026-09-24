e127446b54ff732151d36050c0fc4f6467b792a0
Ashish kumar <ashishkumar29032011@gmail.com>
2026-09-24 08:40:06 +0000

    chore(ci): update instance_type to mac_mini_m2 for free tier

--- src/pages/AssistPage.tsx
+++
@@ -1,9 +0,0 @@
-import { JarvisConfigPage } from './JarvisConfigPage';
-
-interface AssistPageProps {
-  onNavigateBack?: () => void;
-}
-
-export function AssistPage({ onNavigateBack }: AssistPageProps) {
-  return <JarvisConfigPage onNavigateBack={onNavigateBack} />;
-}
